bcab9ce69f550a033ca4a45334606543118b9cf2
runoob <test@runoob.com>
2026-04-02 23:58:56 +0800

    26.04.02更新条目

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     specialDates: [
         { date: "2026-01-21", title: "💕 故事开始的这一天", type: "start" },
         { date: "2026-02-25", title: "💝 第一次心动约会", type: "special" },
-        { date: "2026-05-20", title: "❤️ 久别重逢的拥抱", type: "special" }
+        { date: "2026-04-03", title: "❤️ 久别重逢的拥抱", type: "special" }
 //      { date: "2026-05-20", title: "🌹 520·我爱你", type: "special" }
     ],
     monthlyAnniversary: 21,
